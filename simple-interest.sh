09a057b1ff3d625fd9a59a4352fe33be2b680f84
Md Rahman <110883909+MdM-Rahman@users.noreply.github.com>
2023-11-02 15:08:48 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,7 +5,8 @@
    # Do not use this in production. Sample purpose only.
 
    # Author: Upkar Lidder (IBM)
-   # Additional Authors: MdM-Rahman
+   # Additional Authors: 
+   MdM-Rahman
 
    # Input:
    # p, principal amount
